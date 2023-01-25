13243d98b558df7198352531f56d4e9c73f151ad
Thiago jose da silva <thiago.ads.silva@gmail.com>
2023-01-25 20:07:13 -0300

    Organização

---
+++ EstruturaSequencialExercicioUm/EstruturaSequencialExercicioUm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EstruturaSequencialExercicioUm // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o primeiro valor:");
+            int valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo valor:");
+            int valor2 = int.Parse(Console.ReadLine());
+
+            int resultado = valor1 + valor2;
+
+            Console.WriteLine($"Soma = {resultado}");
+        }
+    }
+}
