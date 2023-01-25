b597481ee64fe23cf7976bdec8423ded2095edbd
Thiago jose da silva <thiago.ads.silva@gmail.com>
2023-01-25 20:06:50 -0300

    Organização

---
+++ EstruturaSequencialExercicioTres/EstruturaSequencialExercicioTres/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstruturaSequencialExercicioTres // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe 4 números para operação:");
+
+            int a, b, c, d;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+            d = int.Parse(vet[3]);
+
+            int resultado = (a * b - c * d);
+
+            Console.WriteLine($"Diferença = {resultado}");
+        }
+    }
+}
