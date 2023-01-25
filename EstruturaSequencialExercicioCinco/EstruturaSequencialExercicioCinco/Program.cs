9194f833395fc2ef3f9e8b96cc61a5bbea085f79
Thiago jose da silva <thiago.ads.silva@gmail.com>
2023-01-25 20:04:57 -0300

    Organização

---
+++ EstruturaSequencialExercicioCinco/EstruturaSequencialExercicioCinco/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaSequencialExercicioCinco // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pecaUm, pecaDois,numeroPecaUm, numeroPecaDois;
+            double valorUnitarioPecaUm, valorUnitarioPecaDois, resultado;
+
+
+            Console.WriteLine("Informe o código de primeira peça:");
+            pecaUm = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o número de primeira peça:");
+            numeroPecaUm = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor unitário de primeira peça:");
+            valorUnitarioPecaUm = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Informe o código de segunda peça:");
+            pecaDois = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o número de segunda peça:");
+            numeroPecaDois = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor unitário de segunda peça:");
+            valorUnitarioPecaDois = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            resultado = (valorUnitarioPecaUm * numeroPecaUm) + (valorUnitarioPecaDois * numeroPecaDois);
+
+            Console.WriteLine($"VALOR A PAGAR: R$ {resultado.ToString("F2")}");
+
+
+        }
+    }
+}
---
+++ EstruturaSequencialExercicioDois/EstruturaSequencialExercicioDois/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaSequencialExercicioDois // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor do raio:");
+            double valorRaio = double.Parse(Console.ReadLine());
+
+            double valorTT = 3.14159;
+            double resultado = valorTT * (valorRaio * valorRaio);
+
+            Console.WriteLine($"A = {resultado.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
