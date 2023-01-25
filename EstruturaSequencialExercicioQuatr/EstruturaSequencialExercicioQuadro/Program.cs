ff2bca8d46bc2e66a29e1d93b59f3ff4bd014924
Thiago jose da silva <thiago.ads.silva@gmail.com>
2023-01-25 20:13:26 -0300

    Organização

---
+++ EstruturaSequencialExercicioQuatr/EstruturaSequencialExercicioQuadro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaSequencialExercicioQuadro // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o número do funcionário:");
+            int numeroFuncionario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantas horas de trabalho por dia?");
+            int horasTabalho = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quanto é o valor da hora");
+            double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = valorHora * horasTabalho;
+
+            Console.WriteLine($"Number = {numeroFuncionario}");
+            Console.WriteLine($"Salary = {resultado.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
